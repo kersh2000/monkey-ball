f8f85d3aef5dd5fdfcebe1ebb6595ef6d817f43a
kersh2000 <bradleykershaw333@gmail.com>
2022-11-29 13:12:24 +0000

    Finalised movement

--- src/scripts/scenes/functions/ballMovements.js
+++ src/scripts/scenes/functions/ballMovements.js
@@ -2,29 +2,29 @@ import { THREE } from "@enable3d/phaser-extension";
 
 export default class Move {
 
-  constructor (keys, ball, camera) {
+  constructor (keys, ball, camera, orBall) {
     const moving = ((keys.left.isDown) || (keys.right.isDown) || (keys.up.isDown) || (keys.down.isDown)) || ((ball.velocity.x > 0.001) || (ball.velocity.y > 0.001) || (ball.velocity.z > 0.001));
     if (moving) {
-      this.addVel(keys, ball, camera);
-    } 
+      this.addVel(keys, ball);
+    }
+    const pos = this.rotate(keys, ball);
+    this.camera(orBall, camera, pos);
   }
 
-  addVel(keys, ball, camera) {
+  addVel(keys, ball) {
 
-    const cords = Object.assign({}, ball.position);
-
-    if (keys.left.isDown) {
+    if (keys.up.isDown) {
       const vel = (ball.velocity.x - 0.025 > -2) ? ball.velocity.x - 0.025 : -2;
       ball.setVelocityX(vel);
-    } else if (keys.right.isDown) {
+    } else if (keys.down.isDown) {
       const vel = (ball.velocity.x + 0.025 < 2) ? ball.velocity.x + 0.025 : 2;
       ball.setVelocityX(vel);   
     }
     
-    if (keys.up.isDown) {
+    if (keys.right.isDown) {
       const vel = (ball.velocity.z - 0.025 > -2) ? ball.velocity.z - 0.025 : -2;
       ball.setVelocityZ(vel);
-    } else if (keys.down.isDown) {
+    } else if (keys.left.isDown) {
       const vel = (ball.velocity.z + 0.025 < 2) ? ball.velocity.z + 0.025 : 2;
       ball.setVelocityZ(vel);
     }
@@ -38,12 +38,39 @@ export default class Move {
       if (Math.abs(z) < 0.3) {
         ball.setVelocityZ(0);
       }
+      if (Math.abs(y) < 0.3) {
+        ball.setVelocityY(0);
+      }
+    }
+  }
+
+  rotate(keys, ball) {
+
+    const zoomFactor = 2;
+    let pos = {};
+
+    if (keys.a.isDown) {
+      pos.x = 0;
+      pos.z = -2.5 * zoomFactor;
+    } else if (keys.d.isDown) {
+      pos.x = 0;
+      pos.z = 2.5 * zoomFactor;
+    } else if (keys.s.isDown) {
+      pos.x = -2.5 * zoomFactor;
+      pos.z = 0;
+    } else {
+      pos.x = 2.5 * zoomFactor;
+      pos.z = 0;
     }
 
-    camera.position.add(new THREE.Vector3((ball.position.x - cords.x), (ball.position.y - cords.y), (ball.position.z - cords.z)));
+    return pos;
 
-    // console.log(camera.position);
+  }
 
+  camera(ball, camera, adj) {
+    const pos = { x: ball.matrixWorld.elements[12], y: ball.matrixWorld.elements[13], z: ball.matrixWorld.elements[14] }
+    camera.position.set(pos.x + adj.x, pos.y + 1.75, pos.z + adj.z);
+    camera.lookAt(new THREE.Vector3( pos.x, pos.y + 0.5, pos.z));
   }
 
 }
--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -1,6 +1,7 @@
 import { Scene3D } from '@enable3d/phaser-extension'
 import Move from './functions/ballMovements';
 import Rotate from './functions/stageRotations';
+import { ThirdPersonControls, THREE } from '@enable3d/phaser-extension';
 
 export default class MainScene extends Scene3D {
   constructor() {
@@ -10,27 +11,66 @@ export default class MainScene extends Scene3D {
   }
 
   init() {
-    this.accessThirdDimension()
+    this.accessThirdDimension();
+    this.third.load.preload('grass', '../../../assets/img/grass.jpg');
   }
 
   create() {
+
+    this.third.physics.debug.enable()
     //importing keyboard inputs for movement of the monkeyball/character
-    this.cursors = this.input.keyboard.createCursorKeys()
+    this.cursors = {
+      up: this.input.keyboard.addKey('up'),
+      down: this.input.keyboard.addKey('down'),
+      right: this.input.keyboard.addKey('right'),
+      left: this.input.keyboard.addKey('left'),
+      a: this.input.keyboard.addKey('a'),
+      w: this.input.keyboard.addKey('w'),
+      d: this.input.keyboard.addKey('d'),
+      s: this.input.keyboard.addKey('s'),
+      space: this.input.keyboard.addKey(32)
+    }
     // creates a nice scene
-    this.third.warpSpeed()
-    this.third.camera.position.set(0, 12, 12);
+    this.third.warpSpeed('light', 'camera', 'sky');
+
+
+    this.third.load.texture('grass').then(grass => {
+      grass.wrapS = grass.wrapT = 1000 // RepeatWrapping
+      grass.offset.set(0, 0)
+      grass.repeat.set(2, 2)
+
+      // BUG: To add shadows to your ground, set transparent = true
+      this.third.physics.add.ground({
+        width: 20,
+        height: 20,
+        y: 1
+      },
+      {
+        phong: {
+          map: grass
+        }
+      })
+    })
 
     // adds a sphere with physics
     this.ball = this.third.physics.add.sphere({ radius: 0.5, x: 0, y:2 }, { lambert: { color: 0xff0000, transparent: true, opacity: 0.5 } }) //position of box init+colours
 
     // this.cameras.main.startFollow(this.ball);
 
+    new Move(this.cursors, this.ball.body, this.third.camera, this.ball);
+
   }
 
   update() {
 
-    new Move(this.cursors, this.ball.body, this.third.camera);
+    new Move(this.cursors, this.ball.body, this.third.camera, this.ball);
+    // const pos = { x: this.ball.matrixWorld.elements[12], y: this.ball.matrixWorld.elements[13], z: this.ball.matrixWorld.elements[14] }
+    // this.third.camera.lookAt(pos.x, pos.y + 3, pos.z)
     // new Rotate(this.cursors, this.ball.body, this.third.scene);
+
+    if (this.ball.body.velocity.x === 0 && this.ball.body.velocity.z === 0) {
+      this.ball.body.setAngularVelocity(0, 0, 0);
+    }
 
   }
 }
